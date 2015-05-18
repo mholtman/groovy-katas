2a8f3d016391eb2f2392980e2b0a710046176fae
Mark Holtman <mholtman@gmail.com>
2015-05-18 13:37:42 -0400

    return a value for quarter

--- vending-machine/src/main/groovy/com/mholtman/domain/CoinValuator.groovy
+++ vending-machine/src/main/groovy/com/mholtman/domain/CoinValuator.groovy
@@ -5,7 +5,12 @@ class CoinValuator {
     private static final Coin quarter = new Coin(weightGrams: 5.670, diameterMm: 24.26, thicknessMm: 1.75)
 
     private List<Coin> validCoins = [quarter]
+
     def Boolean isValidCoin(Coin coin) {
         validCoins.contains(coin)
+    }
+
+    def double valueOf(Coin coin) {
+        return 0.25
     }
 }
--- vending-machine/src/test/groovy/com/mholtman/domain/CoinValuatorSpec.groovy
+++ vending-machine/src/test/groovy/com/mholtman/domain/CoinValuatorSpec.groovy
@@ -6,11 +6,19 @@ class CoinValuatorSpec extends Specification {
 
     final static Coin QUARTER = new Coin(weightGrams: 5.670, diameterMm: 24.26, thicknessMm: 1.75)
 
-    def "can validate quarter"() {
-        setup:
-        def valuator = new CoinValuator()
+    CoinValuator valuator;
 
+    def setup() {
+        valuator = new CoinValuator()
+    }
+
+    def "can validate quarter"() {
         expect:
         valuator.isValidCoin(QUARTER) == true
+    }
+
+    def "returns value of valid coin"() {
+        expect:
+        valuator.valueOf(QUARTER) == 0.25
     }
 }
